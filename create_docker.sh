0618fd97640ba7115abc2438a35eda3ce6ae6577
dennis <Yzw_123456>
2023-07-25 22:32:36 +0800

    debug : 添加dockerfile

---
+++ create_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+dockerPid=`sudo docker ps -a |grep 'cellxgene-fastapi' |awk '{print $1}'`;
+echo "docker pid " $dockerPid;
+sudo docker stop $dockerPid;
+sudo docker rm $dockerPid;
+dockerImage=`sudo docker images|grep 'cellxgene-fastapi'|awk '{print $3}'`;
+echo "docker image" $dockerImage;
+sudo docker rmi $dockerImage;
+sudo docker build -t cellxgene-fastapi:v1.0 .
+sudo docker run -d --restart=always --name cellxgene-fastapi cellxgene-fastapi:v1.0
