c1b7a7fadbcb1fe6b4b4b640bb8013996e92c122
dennis <dyin@capvision.com>
2023-11-24 18:35:47 +0800

    debug : 修改preview端口

--- main.py
+++ main.py
@@ -83,4 +83,4 @@ if __name__ == "__main__":
     async def shutdown_event():
         await consumer.shutdown()
 
-    uvicorn.run(app, host="0.0.0.0", port=5050)
+    uvicorn.run(app, host="0.0.0.0", port=5051)
